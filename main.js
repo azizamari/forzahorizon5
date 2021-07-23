b688fe188f702c27009aa5d1ec1c1c9eb93d9779
azizamari <azizamari2003@gmail.com>
2021-07-23 16:56:44 +0100

    Add responsive sidebar menu

---
+++ main.js
@@ -0,0 +1,7 @@
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width ='250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width ='0px';
+}
